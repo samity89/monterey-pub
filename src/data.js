e87554f661e02e1c87508a94be4c081485b008ae
Samuel Amity <sam.amity@gmail.com>
2022-11-10 14:35:00 -0500

    creating data template

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const appetizers = [
+    {
+        name: "",
+        ingredients: "",
+    }
+]
+
+const flatbreads = [
+    {
+        name: "",
+        ingredients: "",
+    }
+]
+
+const sammiches = [
+    {
+        name: "",
+        ingredients: "",
+    }
+]
+
+const burgers = [
+    {
+        name: "",
+        ingredients: "",
+    }
+]
+
+const entrees = [
+    {
+        name: "",
+        ingredients: "",
+    }
+]
+
+const sides = [
+    {
+        name: "",
+    }
+]
+
+const cocktails = [
+    {
+        name: "",
+        ingredients: "",
+    }
+]
+
+const draftBeer = [
+    {
+        name: "",
+        abv: "",
+        price: "",
+    }
+]
+
+const beer = [
+    {
+        name: "",
+        abv: "",
+        price: "",
+    }
+]
+
+const wine = [
+    {
+        name: "",
+        origin: "",
+        price: "",
+    }
+]
+
+export { 
+    appetizers, 
+    flatbreads, 
+    sammiches, 
+    burgers, 
+    entrees, 
+    sides, 
+    cocktails, 
+    draftBeer, 
+    beer, 
+    wine 
+};
