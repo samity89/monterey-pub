96ba4f2491e056b1d0fa86b17bbcf2a902db6111
Samuel Amity <sam.amity@gmail.com>
2022-11-28 15:18:12 -0500

    Contact component creation

---
+++ src/components/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Contact() {
+  
+  return <div>
+    <h1>Contact Us</h1>
+    <p>Questions or concerns?  Please feel free to message us on our <a href="https://www.instagram.com/themontereypub/">Instagram</a>!</p>
+    <br/>
+    <h2>Pub Squad</h2>
+    <p>join the Pub Squad for updates on events and specials</p>
+    {/* email form and new users */}
+  </div>;
+}
+
+export default Contact;
