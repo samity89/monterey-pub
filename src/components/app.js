4c8952ebc6b5cfd1824f7890b7b86ce9fb13424e
Samuel Amity <sam.amity@gmail.com>
2022-11-28 15:32:40 -0500

    setting client-side routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./navbar";
+import Home from "./home";
+import Food from "./food";
+import Drinks from "./drinks";
+import Contact from "./contact";
+
 
 function App() {
   
   return ( 
     <div>
       <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/drinks" element={<Drinks/>}/>
+        <Route exact path="/food" element={<Food/>}/>
+        <Route exact path="/contact" element={<Contact/>}/>
+      </Routes>
     </div>
   )}
+  
   
   export default App;
