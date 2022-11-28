5a659ae95087efda0076b1f78ae4fbad83aba38b
Samuel Amity <sam.amity@gmail.com>
2022-11-28 15:15:05 -0500

    Drinks Componenet Created

---
+++ src/components/drinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Drinks() {
+
+  return (
+    <div>
+      <h1>Drinks</h1> 
+      <h2>Cocktails</h2>
+      <h2>Draft Beer</h2>
+    </div>
+    );
+}
+
+export default Drinks;
