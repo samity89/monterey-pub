e44f2ba5ed9b1bf5df41fdbbe54365afb88c343b
Samuel Amity <sam.amity@gmail.com>
2022-11-28 17:27:55 -0500

    Drinks rendering

--- src/components/drinks.js
+++ src/components/drinks.js
@@ -1,12 +1,27 @@
 import React from "react";
+import {CocktailCard, BeerCard} from "./drinkcards";
 
-function Drinks() {
-
+function Drinks({ cocktails, beer }) {
+  const renderCocktails = cocktails.map((cocktail) => (
+    <CocktailCard
+      cocktail={cocktail}
+      key={cocktail.name}
+      />
+  ))
+  const renderBeer = beer.map((uniqueBeer) => (
+    <BeerCard
+      beer={uniqueBeer}
+      key={uniqueBeer.name}
+      />
+  ))
+  
   return (
     <div>
       <h1>Drinks</h1> 
       <h2>Cocktails</h2>
+      <div id="cocktailContainer">{renderCocktails}</div>
       <h2>Draft Beer</h2>
+      <div id="beerContainer">{renderBeer}</div>
     </div>
     );
 }
